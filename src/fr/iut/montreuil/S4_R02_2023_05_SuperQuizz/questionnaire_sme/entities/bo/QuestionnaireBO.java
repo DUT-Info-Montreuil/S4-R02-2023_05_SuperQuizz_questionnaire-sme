15d04cacf5391fc08c105b7201a8635cc41cc668
G8LD <steven.yang.ys@gmail.com>
2023-02-14 15:49:04 +0100

    correction des classes QuestionnaireBO, QuestionDTO

--- src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz/questionnaire_sme/entities/bo/QuestionnaireBO.java
+++ src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz/questionnaire_sme/entities/bo/QuestionnaireBO.java
@@ -3,10 +3,12 @@ package fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.questionnaire_sme.entities.bo
 import java.util.List;
 
 public class QuestionnaireBO {
-    private List<int> index;
+    private List<int> idQuestionnaires;
+    private List<int> numQuestions;
     private List<String> langues;
-    private List<String> enonces;
+    private List<String> libelles;
     private List<String> reponse;
-    private List<int> points;
+    private List<int> difficultes;
+    private List<String> details;
     private List<String> sources;
 }
--- src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz/questionnaire_sme/entities/dto/QuestionDTO.java
+++ src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz/questionnaire_sme/entities/dto/QuestionDTO.java
@@ -3,6 +3,6 @@ package fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.questionnaire_sme.entities.dt
 import java.util.List;
 
 public class QuestionDTO {
-    private String enonce;
+    private String libelle;
     private String reponse;
 }
