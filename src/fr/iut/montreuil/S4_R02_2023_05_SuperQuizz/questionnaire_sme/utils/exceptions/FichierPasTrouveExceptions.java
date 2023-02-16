60b5f3b844a7872bc3a2b679af36f8f6ca7bee5c
YanisDj <yanisdjahnit03@gmail.com>
2023-02-16 17:22:32 +0100

    modification de l'accent sur un nom de classe

--- src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz/questionnaire_sme/utils/exceptions/FichierPasTrouveExceptions.java
+++ src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz/questionnaire_sme/utils/exceptions/FichierPasTrouveExceptions.java
@@ -1,4 +1,4 @@
 package fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.questionnaire_sme.utils.exceptions;
 
-public class FichierPasTrouvéExceptions extends Exception{
+public class FichierPasTrouveExceptions extends Exception{
 }
