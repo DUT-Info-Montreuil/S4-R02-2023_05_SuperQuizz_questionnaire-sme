154c387e55c84cf37619a1feb14abe9394c277a0
Steven YANG <steven.yang.ys@gmail.com>
2023-02-27 15:55:30 +0100

    lit le fichier csv et le stock dans un attribut questionnaireBO

--- src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz/questionnaire_sme/impl/ServiceQuestionnaireImpl.java
+++ src/fr/iut/montreuil/S4_R02_2023_05_SuperQuizz/questionnaire_sme/impl/ServiceQuestionnaireImpl.java
@@ -1,7 +1,11 @@
 package fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.questionnaire_sme.impl;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvValidationException;
+import fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.questionnaire_sme.entities.bo.QuestionnaireBO;
 import fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.questionnaire_sme.entities.dto.QuestionnaireDTO;
 import fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.questionnaire_sme.modeles.IServiceQuestionnaire;
 import fr.iut.montreuil.S4_R02_2023_05_SuperQuizz.questionnaire_sme.utils.exceptions.FichierIncorrectExceptions;
@@ -14,6 +18,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ServiceQuestionnaireImpl implements IServiceQuestionnaire {
+
+    private QuestionnaireBO questionnaireBO;
 
     public static void main(String[] args) {
         ServiceQuestionnaireImpl s = new ServiceQuestionnaireImpl();
@@ -28,15 +34,30 @@ public class ServiceQuestionnaireImpl implements IServiceQuestionnaire {
         }
     }
 
-
+    private List<Integer> idQuestionnaires;
+    private List<Integer> numQuestions;
+    private List<String> langues;
+    private List<String> libelles;
+    private List<String> reponses;
+    private List<Integer> difficultes;
+    private List<String> details;
+    private List<String> sources;
     @Override
     public List<QuestionnaireDTO> chargerListeQuestionnaire(String nomFichier) throws FichierIncorrectExceptions, FichierVideExceptions, FichierPasTrouveExceptions {
         try {
-            CSVReader reader = new CSVReader(new FileReader(nomFichier));
-            String[] nextline;
-            while ((nextline = reader.readNext()) != null) {
-                String[] tab = nextline[0].split(";");
-                System.out.println(Arrays.toString(tab));
+            questionnaireBO = new QuestionnaireBO();
+            CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+            CSVReader reader = new CSVReaderBuilder(new FileReader(nomFichier)).withCSVParser(parser).build();
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                questionnaireBO.getIdQuestionnaires().add(Integer.valueOf(nextLine[0].replace('\uFEFF',' ').trim()));
+                questionnaireBO.getNumQuestions().add(Integer.valueOf(nextLine[1]));
+                questionnaireBO.getLangues().add(nextLine[2]);
+                questionnaireBO.getLibelles().add(nextLine[3]);
+                questionnaireBO.getReponses().add(nextLine[4]);
+                questionnaireBO.getDifficultes().add(Integer.valueOf(nextLine[5]));
+                questionnaireBO.getDetails().add(nextLine[6]);
+                questionnaireBO.getSources().add(nextLine[7]);
             }
         } catch (IOException e) {
             e.printStackTrace();
